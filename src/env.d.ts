d512d211c448c6d721fd18848d47ff74f18b0b94
aritraroy24 <aritraroy24@gmail.com>
2023-05-15 12:44:28 +0530

    Initial commit for astro-portfolio with navbar and hero section

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="astro/client-image" />
+interface ImportMetaEnv {
+  readonly GA_TRACKING_ID: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ types/moduletypes.d.ts
@@ -0,0 +1,2 @@
+declare module "react-preloaders2";
+declare module "react-reveal/Fade";
