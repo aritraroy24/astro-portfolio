268908771995daf94d4b970a09da2b6337cf85cb
aritraroy24 <aritraroy24@gmail.com>
2024-12-03 21:11:08 +0000

    og image link modified

--- src/assets/js/data/siteData.js
+++ src/assets/js/data/siteData.js
@@ -2,7 +2,7 @@ const siteData = {
   "title": "Aritra Roy | Computational Materials Scientist",
   "description": "Doctoral student in Materials Science at LSBU, UK. Researching the computational design of materials for energy storage and conversion applications.",
   "cover": {
-    "src": "https://ibb.co/5136hJ0",
+    "src": "https://i.ibb.co/MgHskJv/fullerene.png",
     "alt": "OpenGraph Image for aritraroy.live"
   }
 }
